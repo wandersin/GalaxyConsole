c2a07e18a58ca8386d2b6d94a0aed04f31669f54
wangyunshu <13041202543@163.com>
2021-07-26 21:09:23 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Hello</div>
+    <div id="app">
+        <div id="app-header"></div>
+        <div id="app-lefter"></div>
+    </div>
 </template>
 
 <script>
@@ -9,6 +12,24 @@ export default {
     components: {}
 }
 </script>
-
 <style>
+#app-header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 3rem;
+    width: 100%;
+    border-bottom: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+#app-lefter {
+    position: absolute;
+    top: 3rem;
+    left: 0;
+    height: calc(100% - 3rem);
+    width: 10rem;
+    border-right: 1px solid #ccc;
+    box-sizing: border-box;
+}
 </style>
