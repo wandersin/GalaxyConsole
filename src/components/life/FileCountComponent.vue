aa59e02a724dc142bfa68dd81931e7ae5ed0578c
wangyunshu <13041202543@163.com>
2021-08-02 19:28:19 +0800

    文件数量统计

--- src/components/life/FileCountComponent.vue
+++ src/components/life/FileCountComponent.vue
@@ -1,23 +1,23 @@
 <template>
-    <el-col :span="6">
-        <el-card class="count-card" shadow="hover">
+    <el-col :span="6"  @click.native="selectedThis">
+        <el-card :class="selectedClass" shadow="hover">
             <el-row class="count-card-box">
-                <el-col :span="12" class="count-card-content">
-                    <div class="count-card-content-left">
-                        <div class="content-top">
-                            <span class="number">{{ countFileResult.fileCount }}</span>
-                        </div>
-                        <div class="content-bottom">
-                            <span>{{ title }}</span>
-                        </div>
+                <el-col :span="12" class="count-card-content-left">
+                    <div class="content-top">
+                        <span class="number">{{ countFileResult.fileCount }}</span>
+                    </div>
+                    <div class="content-bottom">
+                        <span>{{ title }}</span>
                     </div>
                 </el-col>
-                <el-col :span="12" class="count-card-content">
-                    <div>大小：{{ fileSize }}</div>
-                    <div>图片：{{ countFileResult.picture }}</div>
-                    <div>音频：{{ countFileResult.music }}</div>
-                    <div>视频：{{ countFileResult.movie }}</div>
-                    <div>其他：{{ countFileResult.other }}</div>
+                <el-col :span="12" class="count-card-content-right">
+                    <div>
+                        <span>大小：{{ fileSize }}</span>
+                        <span>图片：{{ countFileResult.picture }}</span>
+                        <span>音频：{{ countFileResult.music }}</span>
+                        <span>视频：{{ countFileResult.movie }}</span>
+                        <span>其他：{{ countFileResult.other }}</span>
+                    </div>
                 </el-col>
             </el-row>
         </el-card>
@@ -29,8 +29,16 @@ export default {
     name: "FileCountComponent",
     data() {
         return {
-            countFileResult: {},
-            fileSize: ''
+            countFileResult: {
+                fileCount: 0,
+                fileSize: 0,
+                picture: 0,
+                music: 0,
+                movie: 0,
+                other: 0
+            },
+            fileSize: '',
+            selectedClass: 'count-card'
         }
     },
     methods: {
@@ -39,7 +47,14 @@ export default {
                 this.countFileResult = data;
                 this.fileSize = this.$commonUtils.fileSizeTransform(this.countFileResult.fileSize);
             })
-            console.log(this.time);
+        },
+        clearSelection() {
+            this.selectedClass = 'count-card';
+        },
+        // 选择当前card
+        selectedThis() {
+            this.clear(this.time);
+            this.selectedClass = 'count-card selected-card';
         }
     },
     created() {
@@ -47,7 +62,8 @@ export default {
     },
     props: {
         time: Number,
-        title: String
+        title: String,
+        clear: Function
     }
 }
 </script>
@@ -61,17 +77,17 @@ export default {
 
 .count-card:hover {
     cursor: pointer;
+    background-color: white;
+    border: 1px solid #23c6c8;
+}
+
+.selected-card {
+    background-color: white;
+    border: 1px solid #23c6c8;
 }
 
 .count-card-box {
     height: calc(10rem - 40px);
-}
-
-.count-card-content {
-    height: 100%;
-    border: 1px solid lightcyan;
-    box-sizing: border-box;
-    position: relative;
 }
 
 .count-card-content-left {
@@ -79,7 +95,7 @@ export default {
     top: 0;
     left: 0;
     height: 100%;
-    width: 100%;
+    width: 50%;
     text-align: center;
 }
 
@@ -114,5 +130,33 @@ export default {
 
 .count-card-content-left .number {
     font-weight: bolder;
+}
+
+.count-card-content-right {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 50%;
+}
+
+.count-card-content-right div {
+    color: #999;
+    position: absolute;
+    width: 100%;
+    height: min-content;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+}
+
+.count-card-content-right span {
+    font-size: 1rem;
+    width: 100%;
+    float: left;
+    margin: .1rem 0;
+    padding: 0 1rem;
 }
 </style>
--- src/components/life/FileManager.vue
+++ src/components/life/FileManager.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="file-manager-main">
         <el-row :gutter="20">
-            <file-count-component :time="1" :title="'昨日新增'"/>
-            <file-count-component :time="7" :title="'近一周新增'"/>
-            <file-count-component :time="30" :title="'近一月新增'"/>
-            <file-count-component :time="-1" :title="'现有文件'"/>
+            <file-count-component ref="count-card-1" :time="1" :title="'昨日'" :clear="changeTime"/>
+            <file-count-component ref="count-card-7" :time="7" :title="'近一周'" :clear="changeTime"/>
+            <file-count-component ref="count-card-30" :time="30" :title="'近一月'" :clear="changeTime"/>
+            <file-count-component ref="count-card" :time="-1" :title="'现有总数'" :clear="changeTime"/>
         </el-row>
     </div>
 </template>
@@ -21,11 +21,20 @@ export default {
         // 统计文件信息
         countFile(time) {
             console.log(time);
+        },
+        // 切换
+        changeTime(day) {
+            // 清空card样式
+            this.$refs["count-card-1"].clearSelection();
+            this.$refs["count-card-7"].clearSelection();
+            this.$refs["count-card-30"].clearSelection();
+            this.$refs["count-card"].clearSelection();
+            // 加载数据
+            this.countFile(day);
         }
     },
-    created() {
-        // 默认显示昨日新增
-        this.countFile(1);
+    mounted() {
+        this.$refs["count-card-1"].selectedThis();
     }
 }
 </script>
