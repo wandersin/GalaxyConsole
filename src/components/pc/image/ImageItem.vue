6a18591a14b095523e298bc69c120ca8d1fb598e
wangyunshu <13041202543@163.com>
2023-12-25 11:43:17 +0800

    ZEUS-179 添加小图信息显示

--- src/components/pc/image/ImageItem.vue
+++ src/components/pc/image/ImageItem.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="image-show-root" v-loading="loading">
-    <div id="image-show-box" @click="showImageDetail(image)">
-      <el-image v-show="!loading" :src="src" :fit="fit" class="image-item"/>
+    <div id="image-show-box">
+      <div class="image-show-component image-show-header image-hidden-item">
+        <i class="el-icon-info ocr-image-operation-item"></i>
+        <span>{{ image.datetime | dataFormat('YYYY-MM-DD') }}</span>
+      </div>
+      <el-button id="ocr-delete-btn" class="image-hidden-item" icon="el-icon-delete" circle></el-button>
+      <el-button v-if="image.fileType === 'jpeg'" id="ocr-image2url-btn" class="image-hidden-item" icon="el-icon-upload2" circle></el-button>
+      <el-image v-show="!loading" :src="src" :fit="fit" class="image-item" @click="showImageDetail(image)"/>
+      <div class="image-show-component image-show-footer image-hidden-item">{{ image.fileName }}</div>
     </div>
   </div>
 </template>
@@ -67,6 +74,50 @@ export default {
   border-radius: .5rem;
   border: 1px solid lightgray;
   box-sizing: border-box;
+  overflow: hidden;
+}
+
+#image-show-box:hover .image-hidden-item {
+  visibility: inherit;
+}
+
+.image-hidden-item {
+  visibility: hidden;
+}
+
+.image-show-component {
+  height: 15%;
+  width: calc(100% - 1rem);
+  position: absolute;
+  z-index: 99;
+  background: lightgray;
+  display: flex;
+  align-items: center;
+  padding: 0 .5rem;
+  opacity: .9;
+}
+
+#ocr-delete-btn {
+  position: absolute;
+  top: 20%;
+  left: .5rem;
+  z-index: 99;
+}
+
+#ocr-image2url-btn {
+  position: absolute;
+  top: 20%;
+  right: .5rem;
+  z-index: 99;
+}
+
+.image-show-header {
+  top: 0;
+  left: 0;
+}
+
+.ocr-image-operation-item {
+  margin-right: .5rem;
 }
 
 .image-item {
@@ -74,5 +125,14 @@ export default {
   height: calc(100% - 10px);
   border-radius: .5rem;
   margin: 5px;
+}
+
+.image-show-footer {
+  bottom: 0;
+  left: 0;
+  text-align: left;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
