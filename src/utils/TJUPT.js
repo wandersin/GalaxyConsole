ac9ae7e1f2b1473e0adca1324a8cdf9cbb9a2a90
wangyunshu <13041202543@163.com>
2024-10-16 09:57:50 +0800

    新增篡改猴脚本

--- src/utils/TJUPT.js
+++ src/utils/TJUPT.js
@@ -9,36 +9,35 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
-
-    function getUserInfo(env) {
-        console.log(`Current env: ${env}`);
-    }
-
-    getUserInfo('development');
 
     // Start
     console.log('TJUPT AutoDownload Start >>>>>');
-
-    let serviceUrl = 'https://ds1821plus.internal.mrtree.vip:50444';
-    let username = 'wangyunshu';
-    let password = '514232098zx.';
-
-    // 测试环境
-    // let serviceUrl = 'http://ds920plus.internal.mrtree.vip:58080';
-    // let username = 'wangyunshu';
-    // let password = '514232098zx.';
-
-    // 查找所有保存按钮
-    let download = document.querySelectorAll('.download');
-    for(let i = 0; i < download.length; i++) {
-        // 获取种子id
-        let id = '';
-        // 1. 查询所有的a标签
-        let allLinks = download[i].parentElement.parentElement.parentElement.querySelectorAll('a');
+    let data = {
+        "development": {
+            "serviceUrl": "http://ds920plus.internal.mrtree.vip:58080",
+            "username": "wangyunshu",
+            "password": "514232098zx."
+        },
+        "production": {
+            "serviceUrl": "https://ds1821plus.internal.mrtree.vip:50444",
+            "username": "wangyunshu",
+            "password": "514232098zx."
+        }
+    }
+    let env = 'development';
+    // 1. 获取请求token
+    let token = await getToken();
+    // 2. 在指定位置添加按钮
+    let itemTable = document.querySelectorAll('.torrentname');
+    for (let i = 0; i < itemTable.length; i++) {
+        let parent = itemTable[i].querySelector('tr');
+        // 2.1 查询所有的a标签
+        let allLinks = parent.querySelectorAll('a');
+        // 2.2 提取id
         let idRegex = /[?&]id=([^&]*)/;
-        // 2. 提取id
+        let id = '';
         allLinks.forEach(link => {
             let href = link.href;
             let match = href.match(idRegex);
@@ -47,58 +46,79 @@
                 id = match[1]
             }
         });
-        // 创建按钮
-        if (id !== '') {
-            let tmp = download[i].parentElement.parentElement.parentElement.children[1];
-            tmp.setAttribute('style', 'position: relative;');
-            let btn = document.createElement("button");
-            btn.innerHTML = '快速下载';
-            btn.setAttribute('class', 'medium');
-            btn.setAttribute('style', 'position: absolute; top: 0; right: 0; margin: 0; border-radius: 10px; background: lightgray;');
-            btn.onclick = function() {
-                // 获取token
-                GM_xmlhttpRequest({
-                    url: `${serviceUrl}/authman/user/login`,
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    data: JSON.stringify({
-                        "username": username,
-                        "password": password,
-                        "email": ''
-                    }),
-                    onload(tokenResp) {
-                        if (JSON.parse(tokenResp.response).status !== 'ok') {
-                            alert('获取token失败, 请检查后重试');
-                            return;
-                        }
-                        let token = JSON.parse(tokenResp.response).result;
-                        // 添加下载任务
-                        GM_xmlhttpRequest({
-                            url: `${serviceUrl}/core/download/pt/${id}`,
-                            method: 'POST',
-                            headers: {
-                                "Content-Type": "application/json",
-                                "X-Auth-Token": token
-                            },
-                            data: JSON.stringify({
-                                "pt": "TJU",
-                                "synology": "DS1821plus"
-                            }),
-                            onload(downloadResp) {
-                                console.log();
-                                if (JSON.parse(downloadResp.response).status === 'ok') {
-                                    alert('添加下载任务成功');
-                                } else {
-                                    alert('添加下载任务失败, 请检查后重试');
-                                }
-                            }
-                        });
-                    }
-                });
-            }
-            tmp.appendChild(btn);
+        if (id === undefined || id === '') {
+            continue;
         }
+        // 2.3 生成按钮
+        parent.setAttribute('style', 'position: relative;')
+        let btn = document.createElement('button');
+        btn.innerText = '快速下载';
+        btn.setAttribute('class', 'medium');
+        btn.setAttribute('style', 'position: absolute; top: 0; right: 3rem; bottom: 0; height: 2rem; margin: auto; border-radius: 10px; background: #fbe7df;');
+        btn.setAttribute('task-id', id);
+        btn.onmouseover = function() {
+            this.style.cursor = 'pointer';
+        }
+        // 2.4 绑定点击事件
+        btn.onclick = function() {
+            addTask(id);
+        }
+        parent.appendChild(btn);
+    }
+
+    // 获取token
+    async function getToken() {
+        let resp = await httpRequestPromise({
+            url: `${data[env].serviceUrl}/authman/user/login`,
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({
+                "username": data[env].username,
+                "password": data[env].password,
+                "email": ''
+            })
+        });
+        return JSON.parse(resp.responseText).result;
+    }
+
+    // 添加下载任务
+    function addTask(id) {
+        GM_xmlhttpRequest({
+            url: `${data[env].serviceUrl}/core/download/pt/${id}`,
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "X-Auth-Token": token
+            },
+            data: JSON.stringify({
+                "pt": "TJU",
+                "synology": "DS1821plus"
+            }),
+            onload(downloadResp) {
+                if (JSON.parse(downloadResp.response).status === 'ok') {
+                    alert(`添加下载任务成功: ${id}`);
+                } else {
+                    console.log(JSON.parse(downloadResp.response));
+                    alert('添加下载任务失败, 请检查后重试');
+                }
+            }
+        });
+    }
+
+    // 封装异步调用
+    function httpRequestPromise(details) {
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                ...details,
+                onload: (response) => {
+                    resolve(response);
+                },
+                onerror: (error) => {
+                    reject(error);
+                }
+            });
+        })
     }
 })();
