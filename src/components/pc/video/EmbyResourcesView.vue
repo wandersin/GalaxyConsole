ea67f334d6a2cdb3d541260da8fe28edbea60284
wangyunshu <13041202543@163.com>
2024-12-06 16:40:14 +0800

    emby资源管理页面优化

--- src/components/pc/video/EmbyResourcesView.vue
+++ src/components/pc/video/EmbyResourcesView.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="emby-resources-view">
-    <el-row class="emby-resources-operation-row">
-      <el-col :span="4">
-        <el-input v-model="query.name" placeholder="请输入名称进行搜索" @keydown.enter.native="search()"></el-input>
-      </el-col>
-      <el-col :span="1">
-        <el-button @click="search()" plain>搜索</el-button>
-      </el-col>
-    </el-row>
     <el-row class="emby-resources-tb-row">
-      <el-table :data="result.list" border style="width: 100%">
+      <el-table
+          :data="result.list"
+          height="75vh"
+          style="width: 100%"
+          @filter-change="filterHandler"
+          v-loading="query.loading"
+          border
+      >
         <el-table-column prop="id" label="ID" width="100"></el-table-column>
-        <el-table-column prop="internal_name" label="名称" width="250"></el-table-column>
+        <el-table-column prop="internal_name" width="250">
+          <template slot="header" slot-scope="{}">
+            <div class="header-cell">
+              <span v-show="!filters.name.show">名称</span>
+              <i v-show="!filters.name.show" class="el-icon-search header-cell-icon" @click="showNameSearch"></i>
+              <el-input
+                  v-show="filters.name.show"
+                  placeholder="请输入内容"
+                  v-model="query.name"
+                  class="input-with-select"
+                  size="mini"
+                  @keydown.enter.native="filterByName"
+                  clearable
+              >
+                <el-button slot="append" icon="el-icon-search" @click="filterByName"></el-button>
+              </el-input>
+            </div>
+          </template>
+        </el-table-column>
         <el-table-column prop="overview" label="简介"></el-table-column>
-        <el-table-column label="封面图" width="150">
+        <el-table-column label="封面图" width="130">
           <template slot-scope="scope">
             <div class="emby-resources-image-box">
-              <el-image :src="getImageUrl(scope.row.image)" fit="contain" style="width: 100px;" lazy/>
+              <el-image :src="getImageUrl(scope.row.image)" fit="contain" style="width: 100px;"></el-image>
             </div>
           </template>
         </el-table-column>
@@ -27,14 +44,43 @@
             </div>
           </template>
         </el-table-column>
-        <el-table-column prop="adult" label="成人内容" width="50" :formatter="formatAdult"></el-table-column>
-        <el-table-column prop="type" label="类型" width="100"></el-table-column>
-        <el-table-column label="分辨率" width="100">
+        <el-table-column
+            prop="adult"
+            label="成人内容"
+            :filters="filters.adult"
+            :formatter="formatAdult"
+            width="90"
+            column-key="adult"
+        ></el-table-column>
+        <el-table-column
+            prop="type"
+            label="类型"
+            :filters="filters.type"
+            width="100"
+            column-key="type"
+        />
+        <el-table-column label="分辨率" width="90">
           <template slot-scope="scope">
-            <el-tag v-for="item in scope.row.resolution_name" :key="item" type="success" size="small" effect="plain" style="margin: .3rem;">{{ item }}</el-tag>
+            <el-tag
+                v-for="item in scope.row.resolution_name"
+                :key="item"
+                type="success"
+                size="small"
+                effect="plain"
+                style="margin: .3rem;"
+            >
+              {{ item }}
+            </el-tag>
           </template>
         </el-table-column>
-        <el-table-column prop="notice" label="邮件通知" width="50" :formatter="formatNotice"></el-table-column>
+        <el-table-column
+            prop="notice"
+            label="邮件通知"
+            :formatter="formatNotice"
+            :filters="filters.notice"
+            width="90"
+            column-key="notice"
+        ></el-table-column>
         <el-table-column label="加入时间" width="110">
           <template slot-scope="scope">
             <div class="table-time">
@@ -43,6 +89,18 @@
           </template>
         </el-table-column>
       </el-table>
+    </el-row>
+    <el-row class="emby-resources-pagination-row">
+      <el-pagination
+          :current-page.sync="query.page"
+          :page-sizes="rule.num"
+          :page-size="query.size"
+          :total="result.total"
+          @size-change="pageSizeChangeHandler"
+          @current-change="pageChangeHandler"
+          layout="total, sizes, prev, pager, next"
+          background
+      ></el-pagination>
     </el-row>
   </div>
 </template>
@@ -58,37 +116,90 @@ export default {
       query: {
         name: '',
         page: 1,
-        size: 10
+        size: 10,
+        type: '',
+        adult: '',
+        loading: false,
       },
       result: {
         total:0,
         list: []
       },
       filters: {
-
+        name: {
+          show: false
+        },
+        type: [
+          {text: 'MOVIE', value: 'MOVIE'},
+          {text: 'TV', value: 'TV'},
+        ],
+        adult: [
+          {text: '是', value: true},
+          {text: '否', value: false},
+        ],
+        notice: [
+          {text: '是', value: true},
+          {text: '否', value: false},
+        ]
       }
     }
   },
   methods: {
     // 搜索
     search() {
+      this.query.loading = true;
       this.$api.coreVideo.search(this.query).then(data => {
         this.result = data
+        this.query.loading = false;
       })
     },
-    getImageUrl(path) {
-      let url = `${this.$emby_resources_image_endpoint}/t/p/w200${path}`;
-      console.log(url);
-      return url;
+    // 更新搜索条件重新搜索
+    newSearch() {
+      this.query.page = 1;
+      this.search();
     },
+    // 拼接图片链接
+    getImageUrl(path) {
+      return `${this.$emby_resources_image_endpoint}/t/p/w200${path}`;
+    },
+    // boolean值转化为 是/否
     formatBoolean(value) {
       return value === true ? '是' : '否';
     },
+    // 格式化 是否包含成人内容
     formatAdult(row) {
       return this.formatBoolean(row.adult);
     },
+    // 格式化 是否已发送通知
     formatNotice(row) {
       return this.formatBoolean(row.notice);
+    },
+    // 每页显示数量发生变化
+    pageSizeChangeHandler(size) {
+      this.query.size = size;
+      this.search();
+    },
+    // 当前页发生变化
+    pageChangeHandler(page) {
+      this.query.page = page;
+      this.search();
+    },
+    // 筛选
+    filterHandler(filter) {
+      Object.keys(filter).forEach(key => {
+        this.query[key] = filter[key];
+      })
+    },
+    // 根据名称搜索
+    filterByName() {
+      if (this.query.name === '') {
+        this.filters.name.show = false;
+      }
+      this.newSearch();
+    },
+    // 显示名称搜索框
+    showNameSearch() {
+      this.filters.name.show = true;
     }
   },
   created() {
@@ -98,11 +209,6 @@ export default {
 </script>
 
 <style scoped>
-.emby-resources-operation-row {
-  padding: .5rem 0;
-  position: relative;
-}
-
 .emby-resources-image-box {
   position: relative;
   width: 100%;
@@ -110,5 +216,23 @@ export default {
   display: flex;
   justify-content: center; /* 水平居中 */
   align-items: center; /* 垂直居中 */
+}
+
+.emby-resources-pagination-row {
+  margin: 1rem 0;
+}
+
+.header-cell {
+  display: flex;
+  align-items: center;
+  gap: 8px;  /* 设置元素之间的间距 */
+}
+
+.header-cell-icon {
+  font-size: 15px;
+}
+
+.header-cell-icon:hover {
+  cursor: pointer;
 }
 </style>
