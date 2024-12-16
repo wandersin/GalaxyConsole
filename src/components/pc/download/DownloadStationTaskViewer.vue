d1ca56d61115dd5ee681d0100ba0d96876f2448d
wangyunshu <13041202543@163.com>
2024-12-16 11:16:01 +0800

    新增下载状态栏

--- src/components/pc/download/DownloadStationTaskViewer.vue
+++ src/components/pc/download/DownloadStationTaskViewer.vue
@@ -28,6 +28,9 @@
         <el-table-column label="上传速度(每秒)" width="130">
           <template slot-scope="scope">{{ scope.row.additional.transfer.speedUpload | fileSizeFormat }}</template>
         </el-table-column>
+        <el-table-column label="状态" width="100">
+          <template slot-scope="scope">{{ scope.row.status_zh }}</template>
+        </el-table-column>
         <el-table-column label="操作" width="200">
           <template slot="header" slot-scope="{}">
             <div class="header-cell">
